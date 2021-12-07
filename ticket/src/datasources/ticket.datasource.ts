003d5c61b8a995d6af842ccab733db95805f5610
OrlandoRivera95 <OrlandoRivera884@gmail.com>
2021-12-06 21:22:16 -0600

    Actualizacion 5

--- ticket/src/datasources/ticket.datasource.ts
+++ ticket/src/datasources/ticket.datasource.ts
@@ -13,6 +13,8 @@ const config = {
   useNewUrlParser: true
 };
 
+//// me da bastantes errores al actualizar github
+
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
